3dc60135dfe8806350fe9fca2312607990169248
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2026-07-01 08:35:48 +0200

    Release candidate 0 for v2.1

--- src/board/sx126x_hw_api.c
+++ src/board/sx126x_hw_api.c
@@ -9,7 +9,7 @@
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_open(SX126X_HW_API_config_t *hw_api_config) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_UNUSED(hw_api_config);
     SIGFOX_RETURN();
@@ -18,7 +18,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_open(SX126X_HW_API_co
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_close(void) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_RETURN();
 }
@@ -26,7 +26,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_close(void) {
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_delayMs(unsigned short delay_ms) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_UNUSED(delay_ms);
     SIGFOX_RETURN();
@@ -35,7 +35,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_delayMs(unsigned shor
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_chip_name(SX126X_HW_API_chip_name_t *chipset) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_UNUSED(chipset);
     SIGFOX_RETURN();
@@ -44,7 +44,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_chip_name(SX126X_
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_reg_mode(SX126X_HW_API_reg_mod_t *reg_mode) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_UNUSED(reg_mode);
     SIGFOX_RETURN();
@@ -53,7 +53,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_reg_mode(SX126X_H
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_xosc_cfg(SX126X_HW_API_xosc_cfg_t *xosc_cfg) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_UNUSED(xosc_cfg);
     SIGFOX_RETURN();
@@ -62,7 +62,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_xosc_cfg(SX126X_H
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_pa_pwr_cfg(SX126X_HW_API_pa_pwr_cfg_t *pa_pwr_cfg, sfx_u32 rf_freq_in_hz, sfx_s8 expected_output_pwr_in_dbm) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_UNUSED(pa_pwr_cfg);
     SIGFOX_UNUSED(rf_freq_in_hz);
@@ -73,7 +73,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_pa_pwr_cfg(SX126X
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_tx_on(void) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_RETURN();
 }
@@ -81,7 +81,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_tx_on(void) {
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_tx_off(void) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_RETURN();
 }
@@ -90,7 +90,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_tx_off(void) {
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_rx_on(void) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_RETURN();
 }
@@ -100,7 +100,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_rx_on(void) {
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_rx_off(void) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_RETURN();
 }
@@ -110,7 +110,7 @@ SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_rx_off(void) {
 SX126X_HW_API_status_t __attribute__((weak)) SX126X_HW_API_get_latency(SX126X_HW_API_latency_t latency_type, sfx_u32 *latency_ms) {
     /* To be implemented by the device manufacturer */
 #ifdef SIGFOX_EP_ERROR_CODES
-    SX126X_HW_API_status_t status = SX126X_HW_API_SUCCESS;
+    SX126X_HW_API_status_t status = SX126X_HW_API_ERROR;
 #endif
     SIGFOX_UNUSED(latency_type);
     SIGFOX_UNUSED(latency_ms);
